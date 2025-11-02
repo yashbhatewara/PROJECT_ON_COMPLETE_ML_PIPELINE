0b3bf7491716c87e74de72a27180483acdc51315
Yash <yash27bhatewara@gmail.com>
2025-11-02 18:01:08 +0530

    pipe;ine updated

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -28,22 +28,22 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-def load_params(params_path: str) -> dict:
-    """Load parameters from a YAML file."""
-    try:
-        with open(params_path, 'r') as file:
-            params = yaml.safe_load(file)
-        logger.debug('Parameters retrieved from %s', params_path)
-        return params
-    except FileNotFoundError:
-        logger.error('File not found: %s', params_path)
-        raise
-    except yaml.YAMLError as e:
-        logger.error('YAML error: %s', e)
-        raise
-    except Exception as e:
-        logger.error('Unexpected error: %s', e)
-        raise
+# def load_params(params_path: str) -> dict:
+#     """Load parameters from a YAML file."""
+#     try:
+#         with open(params_path, 'r') as file:
+#             params = yaml.safe_load(file)
+#         logger.debug('Parameters retrieved from %s', params_path)
+#         return params
+#     except FileNotFoundError:
+#         logger.error('File not found: %s', params_path)
+#         raise
+#     except yaml.YAMLError as e:
+#         logger.error('YAML error: %s', e)
+#         raise
+#     except Exception as e:
+#         logger.error('Unexpected error: %s', e)
+#         raise
 
 def load_data(data_url: str) -> pd.DataFrame:
     """Load data from a CSV file."""
